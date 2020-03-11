0f4306a4c0ceaec350ded225e7779f23d504096e
Beatrice <beatrice.opara@softwire.com>
2020-03-11 15:20:19 +0000

    Trying to display books from DB

--- Controllers/LibraryController.cs
+++ Controllers/LibraryController.cs
@@ -1,56 +1,60 @@
-﻿using System;
-using System.Diagnostics;
-using Bookish.Models;
-using Microsoft.AspNetCore.Mvc;
-using Bookish.Models;
-using Dapper;
-using Microsoft.Extensions.Logging;
-using System.Data.SqlClient;
+﻿    using System;
+    using System.Diagnostics;
+    using Bookish.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Bookish.Models;
+    using Dapper;
+    using Microsoft.Extensions.Logging;
+    using System.Data.SqlClient;
+    using Microsoft.Extensions.Localization;
 
 
-namespace Bookish.Controllers
-{
-    public class LibraryController : Controller
+    namespace Bookish.Controllers
     {
-        private readonly ILogger<LibraryController> _logger;
-
-        public LibraryController(ILogger<LibraryController> logger)
+        
+        [Route("books")]
+        public class LibraryController : Controller
         {
-            _logger = logger;
-        }
+            private readonly ILogger<LibraryController> _logger;
 
-        // GET
-        public IActionResult Index()
-        {
-
+            public LibraryController(ILogger<LibraryController> logger)
             {
-                var connectionString = "Server=(local);Port=3306;Database=library_mangement;Uid=root;Pwd=Manga239498!";
-                ;
-                //my server, port number, name of database, my id, my pwd
+                _logger = logger;
+            }
 
-                using (SqlConnection connection = new SqlConnection(connectionString))
+            // GET
+            [HttpGet("all")]
+            public IActionResult Index()
+            {
+
                 {
-                    var books = connection.Query<LibraryModel.Books>("Select * From Books ");
+                    var connectionString = "Server=localhost; Port=3306; Database=library_mangement; Uid=root; Pwd=password;";
+                    ;
+                    //my server, port number, name of database, my id, my pwd
 
-                    Console.WriteLine(LibraryModel.Books.);
-                    Console.WriteLine(books);
-                    Console.ReadLine();
-                    return View(books);
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        var books = connection.Query<LibraryModel.Books>("SELECT * FROM Books");
+                        
+                        Console.WriteLine(books);
+                        Console.WriteLine("Hello World");
+                        Console.ReadLine();
+                        return View(books);
+                    }
+
                 }
+            }
 
+
+            public IActionResult Privacy()
+            {
+                return View();
+            }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
             }
         }
-
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-        }
     }
-}
